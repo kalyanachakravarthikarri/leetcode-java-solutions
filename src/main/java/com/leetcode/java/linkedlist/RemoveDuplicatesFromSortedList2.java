ec901709c0c4796731fb4e49b6e50dc74f4ed64d
Kalyan karri <kalyanakarri@microsoft.com>
2023-05-08 22:07:33 +0530

    Remove duplicates from sorted list 2 - without map

--- src/main/java/com/leetcode/java/linkedlist/RemoveDuplicatesFromSortedList2.java
+++ src/main/java/com/leetcode/java/linkedlist/RemoveDuplicatesFromSortedList2.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import com.leetcode.java.linkedlist.utils.ListNode;
 
 public class RemoveDuplicatesFromSortedList2 {
-  public ListNode deleteDuplicates(ListNode head) {
+  public ListNode deleteDuplicatesUsingMap(ListNode head) {
     // handle null and single noded list
     if (head == null) {
       return null;
@@ -15,13 +15,13 @@ public class RemoveDuplicatesFromSortedList2 {
     ListNode result = new ListNode(-1);
     ListNode curr_dest = result;
     Map<Integer, Integer> frequency = new HashMap<>();
-    //populate frequency map
+    // populate frequency map
     while (curr != null) {
       frequency.put(curr.val, frequency.getOrDefault(curr.val, 0) + 1);
       curr = curr.next;
     }
     curr = head;
-    //set the nodes to result
+    // set the nodes to result
     while (curr != null) {
       if (frequency.get(curr.val) == 1) {
         curr_dest.next = new ListNode(curr.val);
@@ -31,5 +31,28 @@ public class RemoveDuplicatesFromSortedList2 {
     }
     // return result
     return result.next;
+  }
+
+  public ListNode deleteDuplicates(ListNode head) {
+    if (head == null)
+      return null;
+
+    ListNode ans = new ListNode();
+    ans.next = head;
+
+    ListNode ptr = ans;
+
+    while (ptr.next != null && ptr.next.next != null) {
+
+      if (ptr.next.val == ptr.next.next.val) {
+        int value = ptr.next.val;
+        while (ptr.next != null && ptr.next.val == value) {
+          ptr.next = ptr.next.next;
+        }
+      } else {
+        ptr = ptr.next;
+      }
+    }
+    return ans.next;
   }
 }

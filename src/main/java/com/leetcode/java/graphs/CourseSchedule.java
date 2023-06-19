a315a99d935a002d180503b7c79be1a11ddc60be
Kalyan karri <kalyanachakravarthikarri@gmail.com>
2023-06-19 06:47:04 +0530

    course schedule - added cache

--- src/main/java/com/leetcode/java/graphs/CourseSchedule.java
+++ src/main/java/com/leetcode/java/graphs/CourseSchedule.java
@@ -7,8 +7,8 @@ import java.util.Map;
 
 public class CourseSchedule {
   public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
     Map<Integer, List<Integer>> adjacencyList = new HashMap<>(numCourses);
+    boolean[] cache = new boolean[numCourses];
     for(int i=0; i<numCourses; i++){
       adjacencyList.put(i, new ArrayList<Integer>());
     }
@@ -20,19 +20,21 @@ public class CourseSchedule {
     int[] visited = new int[numCourses];
     
     for(int i=0; i<numCourses; i++){
-      if(visited[i] == 0 && !dfs(adjacencyList, visited, i)) return false;
+      if(visited[i] == 0 && !dfs(adjacencyList, visited, i, cache)) return false;
     }    
     return true;
   }
 
-  private boolean dfs(Map<Integer, List<Integer>> adj, int[] visited, int i){
+  private boolean dfs(Map<Integer, List<Integer>> adj, int[] visited, int i, boolean[] cache){
     if(visited[i] == 1) return false;
+    if(cache[i]) return true;
     visited[i] = 1;
 
     for(int ad : adj.get(i)){
-      if(!dfs(adj, visited, ad)) return false;
+      if(!dfs(adj, visited, ad, cache)) return false;
     }
     visited[i] = 2;
+    cache[i] = true;
     return true;
   }
 }
